41ae312e416a65fc2a041fcc9ddd57f151b32655
rahul333 <kumarrahulshah3@gmail.com>
2022-03-22 10:56:35 +0530

    tried to a more smooth pop out effect

--- script.js
+++ script.js
@@ -14,14 +14,14 @@ class TicTacToe extends React.Component {
     render() {
             if(!this.state.overImage){
                 return (<a href="./tic_tac_toe.html">
-                <img onMouseEnter={()=> this.mouseHover(true) } onMouseLeave={()=> this.mouseHover(false)} id="TicTacToe" className="image_prop" src="./tic_tac_toe_img.png" alt="Tic_Tac_Toe"/>
+                <img onMouseEnter={()=> this.mouseHover(true) } onMouseLeave={()=> this.mouseHover(false)} id="TicTacToe" className="image_prop_left" src="./tic_tac_toe_img.png" alt="Tic_Tac_Toe"/>
                 </a>);
             }
     
             else{
                 return (
                     <a href="./tic_tac_toe.html">
-                    <video onMouseEnter={()=> this.mouseHover(true) } onMouseLeave={()=> this.mouseHover(false)} src="sample-mp4-file-small.mp4" id="TicTacToe" muted="muted" className="image_prop" autoPlay={true}>
+                    <video onMouseEnter={()=> this.mouseHover(true) } onMouseLeave={()=> this.mouseHover(false)} src="sample-mp4-file-small.mp4" id="TicTacToe" muted="muted" className="image_prop_left" autoPlay={true}>
                     </video></a>
                     
                 );
@@ -44,14 +44,14 @@ class Fall_Blocks extends React.Component {
     render() {
             if(!this.state.overImage){
                 return (<a href="./fall_blocks.html">
-                <img onMouseEnter={()=> this.mouseHover(true) } onMouseLeave={()=> this.mouseHover(false)} id="fallBlocks" className="image_prop" src="./fall_blocks_img.png" alt="Play Fall_Blocks"/>
+                <img onMouseEnter={()=> this.mouseHover(true) } onMouseLeave={()=> this.mouseHover(false)} id="fallBlocks" className="image_prop_right" src="./fall_blocks_img.png" alt="Play Fall_Blocks"/>
                 </a>);
             }
     
             else{
                 return (
                     <a href="./fall_blocks.html">
-                    <video onMouseEnter={()=> this.mouseHover(true) } onMouseLeave={()=> this.mouseHover(false)} src="sample-mp4-file-small.mp4" id="fallBlocks" muted="muted" className="image_prop" autoPlay={true}>
+                    <video onMouseEnter={()=> this.mouseHover(true) } onMouseLeave={()=> this.mouseHover(false)} src="sample-mp4-file-small.mp4" id="fallBlocks" muted="muted" className="image_prop_right" autoPlay={true}>
                     </video></a>
                     
                 );
